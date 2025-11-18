1e4e5aba2e7f472aaa51af22247095873da4df9d
JeremyCS12 <jeremy.ortiz26@compscihigh.org>
2025-11-18 20:02:36 +0000

    chatgptjs

--- chatgpt.js
+++ chatgpt.js
@@ -5,3 +5,31 @@ const apiKey = process.env.apiKey1
 // run your code with node chatgpt.js
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
+let baseURL = "https://api.openai.com/v1/responses"
+
+async function fetchgpt(inp) {
+
+    let options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${apiKey}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-4.1",
+            input: inp
+        })
+
+    }
+
+    const url = `${baseURL}`
+    const response = await fetch(url, options)
+    const data = await response.json()
+    console.log(data.output[0].content[0].text)
+}
+
+console.log("CHATGPT")
+
+let plo = prompt("Ask chatgpt something")
+
+fetchgpt(plo)
